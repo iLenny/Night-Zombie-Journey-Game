c0382d0ea696262599489cb96bef534973027fbe
Leibniz Berihuete <Leibniz Berihuete>
2017-03-05 05:46:37 -0500

    UP and DOWN direction added

--- njzgame/interfaces/Direction.java
+++ njzgame/interfaces/Direction.java
@@ -3,6 +3,8 @@ package njzgame.interfaces;
 public interface Direction {
 	public static final int RIGHT = 0;
 	public static final int LEFT = 1;
+	public static final int UP = 2;
+	public static final int DOWN = 3;
 	public void setDirection(int direction);
 	public int getDirection();
 }
