6a6c686393b63a18a91816ea18e4cae01746dfc1
Leibniz Berihuete <Leibniz Berihuete>
2017-03-05 05:45:49 -0500

    A character player test

--- njzgame/characters/Pepe.java
+++ njzgame/characters/Pepe.java
@@ -15,6 +15,11 @@ public class Pepe extends Character {
 			new Rectangle2D(0, 0, 58, 70),    // 1
 			new Rectangle2D(58, 0, 58, 70),   // 2
 			new Rectangle2D(0, 0, 58, 70),    // 1
+			new Rectangle2D(0, 0, 58, 70),    // 1
+			new Rectangle2D(58, 0, 58, 70),   // 2
+			new Rectangle2D(0, 0, 58, 70),    // 1
+			new Rectangle2D(58, 0, 58, 70),   // 2
+			new Rectangle2D(0, 0, 58, 70),    // 1
 			new Rectangle2D(116, 0, 58, 70), // 3
 			new Rectangle2D(174, 0, 58, 70), // 4
 			new Rectangle2D(238, 0, 58, 70), // 5
@@ -31,13 +36,21 @@ public class Pepe extends Character {
 			
 	};
 	
+	private static final Rectangle2D [] JUMP_SPRITES = {
+			new Rectangle2D(6, 178, 58, 70),    // 1
+			   
+			
+	};
+	
 	public Pepe(String name, int level) {
 		super(name, level);
 		this.getCharacterView().setImage(PEPE_SPRITES_IMG);
 		SpriteHandler standSprites = new SpriteHandler(30, STAND_SPRITES);
 		SpriteHandler walkSprites = new SpriteHandler(10, WALK_SPRITES);
+		SpriteHandler jumpSprites = new SpriteHandler(0, JUMP_SPRITES);
 		this.setStandingSprites(standSprites);
 		this.setWalkSprites(walkSprites);
+		this.setJumpSprites(jumpSprites);
 	}
 	
 	@Override
